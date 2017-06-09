d81b030938688d75c92960f99edde982912ad80d
Chase Allen <callen@smarterhq.com>
2017-06-08 22:22:37 -0400

    22 getBoundingClientRect

---
+++ 22 getBoundingClientRect/follow_along_links.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const follower = document.getElementById(`follower`),
+    links = document.querySelectorAll(`ul li a`);
+
+function showStats() {
+    let boundingRect = this.getBoundingClientRect(),
+        padding = parseInt(getComputedStyle(follower).getPropertyValue(`padding`));
+
+    follower.style.backgroundColor = `rgba(0, 0, 0, .25)`;
+    follower.style.width = `${Math.round(boundingRect.width)}px`;
+    follower.style.height = `${Math.round(boundingRect.bottom - boundingRect.top)}px`;
+    follower.style.top = `${Math.round(boundingRect.top) - padding}px`
+    follower.style.left = `${Math.round(boundingRect.left) - padding}px`
+}
+
+links.forEach(ea => ea.addEventListener(`mouseenter`, showStats));
